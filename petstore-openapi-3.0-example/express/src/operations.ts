ac2ce660dea99376fb3678d298483464b7c4a45b
Timo Rüppell <timo.ruppell@gmail.com>
2023-04-23 08:15:50 +0000

    Update

--- petstore-openapi-3.0-example/express/src/operations.ts
+++ petstore-openapi-3.0-example/express/src/operations.ts
@@ -8,9 +8,7 @@
 //
 // /////////////////////////////////////////////////////////////////////////////
 import _ from "lodash";
-
 import { Request, Response } from "express";
-
 import data from "./data";
 
 const addPet = (req: Request, res: Response) => {
@@ -24,6 +22,7 @@ const addPetBroken = (req: Request, res: Response) => {
     req.body.id = _.maxBy(data.pets, "id").id + 1;
     req.body.ownerId = req.user.userId;
     data.pets = [...data.pets, _.clone(req.body)];
+
     const brokenResponse = {
         ...req.body,
         id: "This should be an int",
@@ -34,9 +33,6 @@ const addPetBroken = (req: Request, res: Response) => {
 
 const getPetById = (req: Request, res: Response) => {
     const id = +req.params.petId;
-    if (!_.find(data.pets, { id })) {
-        return res.status(404).send("Pet not found");
-    }
     return res.status(200).json(_.find(data.pets, { id }));
 };
 
