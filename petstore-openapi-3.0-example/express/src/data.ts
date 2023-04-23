60e04096175e09543ff5cdb7bae3237b4dc30523
Timo Rüppell <timo.ruppell@gmail.com>
2023-04-23 08:50:43 +0000

    Update

--- petstore-openapi-3.0-example/express/src/data.ts
+++ petstore-openapi-3.0-example/express/src/data.ts
@@ -12,34 +12,15 @@ const pets = [
                 name: "tag2",
             },
         ],
-        ownerId: 0,
+        ownerId: 1,
     },
     {
         id: 12,
-        name: "dog 1",
-        tags: [
-            {
-                id: 2,
-                name: "tag2",
-            },
-            {
-                id: 9,
-                name: "tag9",
-            },
-        ],
-        ownerId: 0,
-    },
-    {
-        id: 13,
         name: "cat 2",
         tags: [
             {
                 id: 3,
                 name: "tag3",
-            },
-            {
-                id: 4,
-                name: "tag4",
             },
         ],
         ownerId: 1234567890,
